1070f86511ddcab6b7476fb19147f7fb5ac82017
Jian Kristian Gapatan <jiankeg09@gmail.com>
2024-09-25 02:49:51 +0800

    Added script for show password

---
+++ register/register.js
@@ -0,0 +1,29 @@
+document.getElementById('eye1').addEventListener('click', function () {
+    const passwordInput = document.getElementById('psw');
+    const eyeIcon = document.getElementById('eye1');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+});
+
+document.getElementById('eye2').addEventListener('click', function () {
+    const passwordInput = document.getElementById('cpsw');
+    const eyeIcon = document.getElementById('eye2');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+});
