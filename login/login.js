3e40e0564cf9d7e139144946504bd11e2a77fe14
Jian Kristian Gapatan <jiankeg09@gmail.com>
2024-09-25 00:58:21 +0800

    register

---
+++ login/login.js
@@ -0,0 +1,14 @@
+document.getElementById('eye').addEventListener('click', function () {
+    const passwordInput = document.getElementById('psw');
+    const eyeIcon = document.getElementById('eye');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+});
